6af041ea4d05625e79aead0d72419ca983555f32
Rafcha <85578143+Rafcha7957@users.noreply.github.com>
2025-02-04 21:43:10 +0500

    Update ip.rs

--- src/modules/ip.rs
+++ src/modules/ip.rs
@@ -4,12 +4,10 @@ pub fn get_ip_address() -> String {
     match get_if_addrs() {
         Ok(interfaces) => {
             for interface in interfaces {
-                // Пропускаем loopback-интерфейсы
                 if interface.is_loopback() {
                     continue;
                 }
 
-                // Берем первый IPv4 адрес
                 if let IfAddr::V4(addr) = interface.addr {
                     return addr.ip.to_string();
                 }
